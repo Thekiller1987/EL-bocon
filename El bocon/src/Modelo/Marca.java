033b015f9defb1ad5eb37f7c83a75e3306200b67
MarceloMartinez7 <mr5405750@gmail.com>
2023-05-18 12:47:52 -0600

    Anadir clase Marca

---
+++ El bocon/src/Modelo/Marca.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author mr540
+ */
+public class Marca {
+
+int id_marca;
+String nombre;
+
+    public int getId_marca() {
+        return id_marca;
+    }
+
+    public void setId_marca(int id_marca) {
+        this.id_marca = id_marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Marca(int id_marca, String nombre) {
+        this.id_marca = id_marca;
+        this.nombre = nombre;
+    }
+    
+}
